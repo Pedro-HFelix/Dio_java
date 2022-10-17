837a4f32c7a206994c6bec84fc3738422d8566bf
Pedro-HFelix <pf711363@gmail.com>
2022-10-17 14:48:11 -0300

    permitir

--- projetos/operadores/operadores.java
+++ projetos/operadores/operadores.java
@@ -3,8 +3,10 @@ public class operadores {
         int a , b;
         a=8;
         b=8;
-        String resultador= a==b?"verdadeiro":"falso";
+      //  String resultador= a==b?"verdadeiro":"falso";
        // resultador = a==b?"verdadeiro":"falso";
+       //com numeros
+       int resultador= a==b? 1 : 2;
        System.out.println(resultador);
     }
 }
